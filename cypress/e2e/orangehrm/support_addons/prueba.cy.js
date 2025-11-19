524510cdf6469af7a6159febe3815dad018a8100
Fernando José Barraza Álvarez <fernando.jba.2000@gmail.com>
2025-11-19 16:57:45 -0600

    Limpieza: Eliminando ejemplos de Cypress del historial y node modules

--- cypress/e2e/orangehrm/support_addons/prueba.cy.js
+++
@@ -1,8 +0,0 @@
-describe('Prueba básica de carga', () => {
-  it('Carga la página principal', () => {
-    cy.visit('https://orangehrm-demo-7x.orangehrmlive.com/', {
-  		failOnStatusCode: false
-	});
-    cy.contains('Advanced Trial').should('be.visible');
-  });
-});
