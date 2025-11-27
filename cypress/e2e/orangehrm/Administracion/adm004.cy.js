be25081ab6654d7d3ba625bfd628f47c567a5617
locofer12 <nando.valdes123@gmail.com>
2025-11-27 11:22:03 -0600

    adm 004 configurado y funcional

--- cypress/e2e/orangehrm/Administracion/adm004.cy.js
+++ cypress/e2e/orangehrm/Administracion/adm004.cy.js
@@ -3,28 +3,32 @@ describe('Bloquear usuario', () => {
     cy.viewport(1009, 873);
 
     // Asume que ya estás autenticado o que tu sesión está activa por cookies
-    cy.visit('https://orangehrm-demo-7x.orangehrmlive.com/client/#/admin/systemUsers');
+    cy.contains(' Administra', { timeout: 25000 }).should('be.visible').click();
 
     // Verificar que estamos en la pantalla correcta
     cy.url().should('include', '/admin/systemUsers');
 
-    // Seleccionar el checkbox de la fila 4 de la tabla
-    cy.get('tr:nth-of-type(4) label')
-      .should('be.visible')
-      .click();
 
-    // Click en el ícono de editar de la fila seleccionada
-    cy.get('tr.grey i')
-      .should('be.visible')
-      .click();
+    cy.get('td.edit_item[data-tooltip="Edit"]', { timeout: 60000 }).first().click();
+    cy.contains(' Disabled', { timeout: 25000 }).should('be.visible').click();
+
+
+
+    // // Seleccionar el checkbox de la fila 4 de la tabla
+    // cy.get('tr:nth-of-type(4) label')
+    //   .should('be.visible')
+    //   .click();
+
+    // // Click en el ícono de editar de la fila seleccionada
+    // cy.get('tr.grey i')
+    //   .should('be.visible')
+    //   .click();
 
     // Guardar cambios en el modal (por ejemplo, bloquear/cambiar estado)
-    cy.get('#modal-save-button')
-      .should('be.visible')
-      .click();
+    //cy.contains(' Save', { timeout: 25000 }).should('be.visible').click();
+    cy.get('button#modal-save-button.btn.btn-secondary', { timeout: 10000 })
+      .should('exist')
+      .click({ force: true });
 
-    // Validar que hubo algún mensaje de éxito (ajusta el texto si es diferente)
-    cy.contains(/success|saved|guardado|creado/i, { timeout: 10000 })
-      .should('exist');
   });
 });
